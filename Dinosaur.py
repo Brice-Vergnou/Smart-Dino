4f8a0d9d1cc9944820e867724a34a5a13a0af91d
Brice-Vergnou <brice.vergnou@gmail.com>
2021-07-01 16:21:42 +0200

    added dunk and jump mechanics

--- Dinosaur.py
+++ Dinosaur.py
@@ -3,18 +3,21 @@ import os
 
 RUNNING_DINO = [image.load(os.path.join("img/Dino/DinoRun1.png")),
                 image.load(os.path.join("img/Dino/DinoRun2.png"))]
-JUMPING_DINO = [image.load(os.path.join("img/Dino/DinoJump.png"))]
-START_DINO = [image.load(os.path.join("img/Dino/DinoStart.png"))]
-DEAD_DINO = [image.load(os.path.join("img/Dino/DinoDead.png"))]
+JUMPING_DINO = image.load(os.path.join("img/Dino/DinoJump.png"))
+START_DINO = image.load(os.path.join("img/Dino/DinoStart.png"))
+DEAD_DINO = image.load(os.path.join("img/Dino/DinoDead.png"))
 DUCKING_DINO = [image.load(os.path.join("img/Dino/DinoDuck1.png")),
                 image.load(os.path.join("img/Dino/DinoDuck2.png"))]
 
 class Dinosaur:
 
     def __init__(self):
-        self.x = 500
-        self.y = 500
+        self.X = 160  # No variance for x
+        self.Y = 360
+        self.y_duck = 394 # We add 34 pixels down ( that's how Pygame works ) because the image is 34px less high
         self.step = 0
+        self.actual_y = self.Y # At the beginning , the dinosaur is at the running height
+        self.jumping_speed = 1
 
         self.run_img = RUNNING_DINO
         self.duck_img = DUCKING_DINO
@@ -29,7 +32,7 @@ class Dinosaur:
 
     def update(self,input):
 
-        # self.hitbox = self.image.get_rect()
+        self.hitbox = self.image.get_rect()
 
         if self.is_ducking:
             self.duck()
@@ -43,29 +46,36 @@ class Dinosaur:
         if self.step >= 8:
             self.step = 0
 
-        if input[K_UP]:
+        if input[K_UP]and not self.is_jumping:  # The dino is jumping if we press the up key
             self.is_jumping = True
             self.is_ducking = False
             self.is_running = False
-        elif input[K_DOWN] and not self.is_jumping:
+        elif input[K_DOWN] and not self.is_jumping: # The dino is ducking if we press the down key and we're not jumping
             self.is_jumping = False
             self.is_ducking = True
             self.is_running = False
-        elif not input[K_DOWN]:
+        elif not ( input[K_DOWN] or self.is_jumping):  # The dino is running if we don't press down and he's not jumping
             self.is_jumping = False
             self.is_ducking = False
             self.is_running = True
 
     def run(self):
-        self.image = self.run_img[self.step // 4]
-        self.x += 5
+        self.image = self.run_img[self.step // 4]  # Switch between running images to animate the dino
+        self.actual_y = self.Y
 
     def duck(self):
-        pass
+        self.image = self.duck_img[self.step // 4]
+        self.actual_y = self.y_duck
 
     def jump(self):
-        pass
+        self.image = self.jump_img
+        print(self.jumping_speed)
+        if self.is_jumping:
+            self.actual_y -= self.jumping_speed * 30
+            self.jumping_speed -= 0.1
+        if self.jumping_speed < - 1 :
+            self.jumping_speed = 1
+            self.is_jumping = False
 
     def draw_image(self,screen):
-        screen.blit(self.image,(self.x,self.y))
-        print(self.image,self.step)
+        screen.blit(self.image,(self.X,self.actual_y))
--- main.py
+++ main.py
@@ -15,16 +15,13 @@ WIDTH = 1280  # 16:9 format
 BACKGROUND = (247,247,247)
 SCREEN = pygame. display. set_mode((WIDTH, HEIGHT))
 
-################# INIT PYGAME ##############################
-
-
 
 ############### LOADING IMAGES ###########################
 
 
-CLOUD = [pygame.image.load(os.path.join("img/Other/Cloud.png"))]
+CLOUD = pygame.image.load(os.path.join("img/Other/Cloud.png"))
 GAME_OVER = [pygame.image.load(os.path.join("img/Other/GameOver.png"))]
-RESET = [pygame.image.load(os.path.join("img/Other/Reset.png"))]
+RESET = pygame.image.load(os.path.join("img/Other/Reset.png"))
 BIRDS = [pygame.image.load(os.path.join("img/Bird/Bird1.png")),
         pygame.image.load(os.path.join("img/Bird/Bird2.png"))]
 LARGE_CACTUS = [pygame.image.load(os.path.join("img/Cactus/LargeCactus1.png")),
@@ -33,12 +30,7 @@ LARGE_CACTUS = [pygame.image.load(os.path.join("img/Cactus/LargeCactus1.png")),
 SMALL_CACTUS = [pygame.image.load(os.path.join("img/Cactus/SmallCactus1.png")),
                 pygame.image.load(os.path.join("img/Cactus/SmallCactus2.png")),
                 pygame.image.load(os.path.join("img/Cactus/SmallCactus3.png"))]
-TRACK = [pygame.image.load(os.path.join("img/Other/Track.png"))]
-
-
-
-pygame.display.set_caption("Smart Dino")
-pygame.display.flip()
+TRACK = pygame.image.load(os.path.join("img/Other/Track.png"))
 
 
 def main():
@@ -46,17 +38,19 @@ def main():
     RUNNING = True
     dino = Dinosaur()
     CLOCK = pygame.time.Clock()
+    pygame.display.set_caption("Smart Dino")
+    pygame.display.flip()
 
     while RUNNING:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 RUNNING = False
         input = pygame.key.get_pressed()
-        SCREEN.fill(BACKGROUND)
+        SCREEN.fill(BACKGROUND)   # We place it here to overwrite the old dino image
 
         dino.draw_image(SCREEN)
         dino.update(input)
 
-        CLOCK.tick(30)
+        CLOCK.tick(30)  # Refreshing
         pygame.display.update()
 main()
