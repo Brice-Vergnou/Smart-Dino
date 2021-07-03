1462edc5b9442fc2f7a286a5e156c0b9c63c5abe
Brice-Vergnou <brice.vergnou@gmail.com>
2021-07-03 11:57:22 +0200

    Cleaned files and added requirements.txt

---
+++ Scripts/Cloud.py
@@ -0,0 +1,19 @@
+import random
+import pygame
+import os
+
+CLOUD = pygame.image.load(os.path.join("../img/Other/Cloud.png"))
+
+
+class Cloud:
+
+    def __init__(self):
+        self.X = 1280 + random.randint(800, 1000)  # So it spawns outside of the screen , 1280 being the width
+        self.Y = random.randint(30, 80)  # Random height
+        self.image = CLOUD
+
+    def update(self, speed):
+        self.X -= speed
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.X, self.Y))
